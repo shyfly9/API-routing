0e6b7741c5bb3eae139c109c0fb0ee7abfb8a0a6
shyam <yash.shyam007@gmail.com>
2023-01-21 14:30:59 +0000

    API routing

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+app.get("/about", (request, response) => {
+  response.send("Home Page");
+});
+module.exports = app;
